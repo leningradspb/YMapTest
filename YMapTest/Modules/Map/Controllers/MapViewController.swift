912ecd8dcaf7f8385f2c5573e43f8fc4f496475f
Eduard Kanevsky <kanevskii@yahoo.com>
2024-05-23 20:06:38 +0300

    Map added

--- YMapTest/Modules/Map/Controllers/MapViewController.swift
+++ YMapTest/Modules/Map/Controllers/MapViewController.swift
@@ -10,6 +10,7 @@ import YandexMapsMobile
 import SnapKit
 
 final class MapViewController: UIViewController {
+    private let mapView = YMKMapView(frame: .zero)!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +19,24 @@ final class MapViewController: UIViewController {
     }
 
     private func setupUI() {
-        view.backgroundColor = .green
+        view.backgroundColor = .black
+        setupMap()
+    }
+    
+    private func setupMap() {
+        view.addSubview(mapView)
+        mapView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        mapView.mapWindow.map.move(
+                with: YMKCameraPosition(
+                    target: YMKPoint(latitude: 59.935493, longitude: 30.327392),
+                    zoom: 15,
+                    azimuth: 0,
+                    tilt: 0
+                ),
+                animation: YMKAnimation(type: YMKAnimationType.smooth, duration: 5),
+                cameraCallback: nil)
     }
 
 }
