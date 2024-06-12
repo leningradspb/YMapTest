6d5cae9282f128f5cc3b20ed4474a501e0732e7f
Ed <Kanevsky>
2024-06-12 10:48:13 +0500

    StartModalOnMapVC in progress

---
+++ YMapTest/Modules/Map/Controllers/StartModalOnMapVC.swift
@@ -0,0 +1,21 @@
+//
+//  StartModalOnMapVC.swift
+//  YMapTest
+//
+//  Created by Eduard Kanevskii on 12.06.2024.
+//
+
+import UIKit
+
+final class StartModalOnMapVC: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+}
